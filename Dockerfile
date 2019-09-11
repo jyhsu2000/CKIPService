ca53d7b6d648c7694ad99eb41095c1007e7f13c6
KID <jyhsu2000@gmail.com>
2019-09-11 14:39:39 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+#FROM tensorflow/tensorflow:nightly-py3
+FROM python:3
+LABEL MAINTAINER="jyhsu2000@gmail.com"
+
+WORKDIR /usr/local/src/CKIPTagger
+
+RUN pip3 --no-cache-dir install \
+	Flask \
+    ckiptagger[tf,gdown]
+
+COPY ckip_service.py ./
+
+EXPOSE 5005
+
+CMD ["python3", "./ckip_service.py"]
---
+++ ckip_service.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# -*- coding: UTF-8 -*-
+import os
+import sys
+from flask import Flask, jsonify, request, send_file
+
+# Suppress as many warnings as possible
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+# from tensorflow.python.util import deprecation
+# deprecation._PRINT_DEPRECATION_WARNINGS = False
+# import tensorflow as tf
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+from ckiptagger import data_utils, construct_dictionary, WS, POS, NER
+
+# model variables
+ws = None
+pos = None
+ner = None
+
+app = Flask(__name__)
+
+
+def initial():
+    global ws, pos, ner
+    # Load model
+    ws = WS("./data")
+    pos = POS("./data")
+    ner = NER("./data")
+
+
+@app.route('/', methods=['POST'])
+def main():
+    global ws, pos, ner
+
+    # if not request.is_json:
+    #     return 'Invalid input'
+    #     exit(1)
+    sentence_list = request.form.get('sentence_list')
+    if sentence_list is None:
+        return 'Missing sentence_list'
+        exit(1)
+    sentence_list = sentence_list.split('\n')
+
+    # Download data
+    # data_utils.download_data("./")
+
+    # Load model
+    # ws = WS("./data")
+    # pos = POS("./data")
+    # ner = NER("./data")
+
+    # Create custom dictionary
+    # word_to_weight = {
+    #     "土地公": 1,
+    #     "土地婆": 1,
+    #     "公有": 2,
+    #     "": 1,
+    #     "來亂的": "啦",
+    #     "緯來體育台": 1,
+    # }
+    # dictionary = construct_dictionary(word_to_weight)
+    # print(dictionary)
+
+    # Run WS-POS-NER pipeline
+    # sentence_list = [
+    #     "傅達仁今將執行安樂死，卻突然爆出自己20年前遭緯來體育台封殺，他不懂自己哪裡得罪到電視台。",
+    #     "美國參議院針對今天總統布什所提名的勞工部長趙小蘭展開認可聽證會，預料她將會很順利通過參議院支持，成為該國有史以來第一位的華裔女性內閣成員。",
+    #     "",
+    #     "土地公有政策?？還是土地婆有政策。.",
+    #     "… 你確定嗎… 不要再騙了……",
+    #     "最多容納59,000個人,或5.9萬人,再多就不行了.這是環評的結論.",
+    #     "科長說:1,坪數對人數為1:3。2,可以再增加。",
+    # ]
+    word_sentence_list = ws(sentence_list)
+    # word_sentence_list = ws(sentence_list, sentence_segmentation=True)
+    # word_sentence_list = ws(sentence_list, recommend_dictionary=dictionary)
+    # word_sentence_list = ws(sentence_list, coerce_dictionary=dictionary)
+    pos_sentence_list = pos(word_sentence_list)
+    entity_sentence_list = ner(word_sentence_list, pos_sentence_list)
+
+    # Release model
+    # del ws
+    # del pos
+    # del ner
+
+    # Show results
+    result = []
+
+    def print_word_pos_sentence(word_sentence, pos_sentence):
+        assert len(word_sentence) == len(pos_sentence)
+        word_pos_sentence = ""
+        for word, pos in zip(word_sentence, pos_sentence):
+            word_pos_sentence += f"{word}({pos})" + "\u3000"
+        return word_pos_sentence
+
+    for i, sentence in enumerate(sentence_list):
+        # result.append("")
+        # result.append(f"'{sentence}'")
+        result.append(print_word_pos_sentence(
+            word_sentence_list[i],  pos_sentence_list[i]))
+        for entity in sorted(entity_sentence_list[i]):
+            result.append(str(entity))
+        result.append("")
+    return '\n'.join(result)
+
+
+if __name__ == "__main__":
+    initial()
+    app.run(debug=False, host='0.0.0.0', port=5005)
+    sys.exit()
