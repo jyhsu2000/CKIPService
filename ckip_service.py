d9cd91dfd1983d78a44c24a454a7a826ad810a9c
Zero <john.royer@gmail.com>
2024-04-30 11:17:48 +0800

    re-format NER data

--- ckip_service.py
+++ ckip_service.py
@@ -80,9 +80,17 @@ async def tokenize(
         )
 
         for entity in sorted(entity_sentence_list[i]):
-            sentence_result['entities'].append(str(entity))
-            result.append(str(entity))
+            sentence_result['entities'].append({
+                'word': entity[3],
+                'type': entity[2],
+                'start': entity[0],
+                'end': entity[1],
+            })
+
+        result.append("")
         json_response['sentences'].append(sentence_result)
+
+    print(result, "\n")
     return json.dumps(json_response)
 
 
