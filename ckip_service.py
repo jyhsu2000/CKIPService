5270ead935e96eb9b80d5cbcef8c8d2f0879afb2
Zero <john.royer@gmail.com>
2024-05-03 11:28:57 +0800

    remove JSON convertor

--- ckip_service.py
+++ ckip_service.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # coding=utf-8
 # -*- coding: UTF-8 -*-
-import json
-
 import uvicorn
 from ckiptagger import WS, POS, NER
 from fastapi import FastAPI
@@ -88,7 +86,7 @@ async def tokenize(
 
         json_response['sentences'].append(sentence_result)
 
-    return json.dumps(json_response)
+    return json_response
 
 
 if __name__ == "__main__":
