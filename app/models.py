e1eb11f0f2f4bf858c848088ca86727d0bf0c4cf
jyhsu <jyhsu2000@gmail.com>
2024-05-04 01:42:36 +0800

    Add some descriptions for readability

--- app/models.py
+++ app/models.py
@@ -3,14 +3,14 @@ from pydantic import BaseModel, Field
 
 class Segment(BaseModel):
     word: str
-    pos: str
+    pos: str = Field(description='Part of speech')
 
 
 class NamedEntity(BaseModel):
     word: str
-    type: str
-    start: int
-    end: int
+    type: str = Field(description='Entity type')
+    start: int = Field(description='Start position')
+    end: int = Field(description='End position')
 
 
 class Sentence(BaseModel):
